5298a4ce9e0aa976fc0d49c908d71133754859c3
R1muru <aholicktbr1798@gmail.com>
2026-07-14 18:25:06 -0300

    Delete bracket-hub-main_v2/projeto final POO directory

--- bracket-hub-main_v2/projeto final POO/sgg_core/excecoes.py
+++
@@ -1,7 +0,0 @@
-# sgg_core/excecoes.py
-
-class RegraNegocioError(Exception):
-    """Exceção lançada quando uma regra de negócio do torneio é violada."""
-    def __init__(self, mensagem):
-        super().__init__(mensagem)
-        self.mensagem = mensagem
